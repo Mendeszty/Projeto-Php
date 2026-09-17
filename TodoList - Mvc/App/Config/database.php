007b8fbb2e50d3c67fbb65b34a15ac218ee63763
PAULO MENDES DE SOUSA NETO <20221134010012@ifrn.local>
2026-09-17 10:19:01 -0300

    Subindo controller e model

---
+++ TodoList - Mvc/App/Config/database.php
@@ -0,0 +1,21 @@
+<?php
+
+class Database {
+    private $host = "localhost";
+    private $usuario = "root";
+    private $senha = "";
+    private $banco = "todolist";
+    private $conn = "";
+
+   
+    public function conectar() {
+    $this->conn = new mysqli($this -> host, $this -> usuario, $this -> senha, $this -> banco);
+    if($this->conn->connect_error){
+        die("Algo deu errado". $this->conn->connect_error);
+
+    }
+    return $this->conn;
+   }
+}
+
+?>
